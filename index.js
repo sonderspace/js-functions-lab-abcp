70f9f980891ae9a557f6da5844aad17607a94976
Efrain Lopez <lopezefrain5894@gmail.com>
2018-01-11 01:28:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+function happyHolidays()
+{
+  //the value of happyHolidays will be equivalent to 'Happy holidays!'
+  return 'Happy holidays!';
+}
+function happyHolidaysTo(name)
+{
+  // the value of happyHolidaysTo will be equivalent to 'Happy holidays, you!'
+  // we use a parameter
+  return "Happy holidays, " + name + "!";
+}
+function happyHolidayTo(holiday, name)
+{
+  return "Happy " + holiday + ", " + name + "!";
+}
+function holidayCountdown(holiday, days)
+{
+  let qtyOfDays = days.toString();
+  return "It's " + qtyOfDays + " days until " + holiday + "!";
+}
